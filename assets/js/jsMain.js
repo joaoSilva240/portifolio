073da5e76e3ec602a8cd19f4f45137426ae611db
joao <joao.silvaoliver240@gmail.com>
2024-08-02 13:34:50 -0300

    incluindo js e responsividade

---
+++ assets/js/jsMain.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('habilidades');
+const Info = document.getElementsByClassName("info")
+button.addEventListener('click', (e) => {
+    debugger
+    const I=Info.button;
+    console.log(Info);
+})
+
+if (open) {
+
+}
