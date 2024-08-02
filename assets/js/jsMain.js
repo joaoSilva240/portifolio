1991c41d5155b63e584cc23aee4deafa5c9991b6
joao <joao.silvaoliver240@gmail.com>
2024-08-02 14:00:49 -0300

    organização v4

--- assets/js/jsMain.js
+++ assets/js/jsMain.js
@@ -1,7 +1,6 @@
 const button = document.getElementById('habilidades');
 const Info = document.getElementsByClassName("info")
 button.addEventListener('click', (e) => {
-    debugger
     const I=Info.button;
     console.log(Info);
 })
